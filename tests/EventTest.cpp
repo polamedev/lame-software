dab4932e6e678e5ab883c127ba009ab454c74850
polame <polame.dev@gmail.com>
2025-10-25 18:26:59 +0300

    Написал все тесты для Event

--- tests/EventTest.cpp
+++ tests/EventTest.cpp
@@ -10,7 +10,7 @@ LAME_Event event;
 
 void setup()
 {
-    
+    LAME_Event_Init(&event);
 }
 
 void teardown()
@@ -21,12 +21,44 @@ void teardown()
 
 TEST(EventTests, clearAfterCreate)
 {
-    LAME_Event_Init(&event);
+    CHECK_FALSE(LAME_Event_Check(&event));
+}
 
+TEST(EventTests, checkEventSet)
+{
+    LAME_Event_Set(&event);
+
+    CHECK_TRUE(LAME_Event_Check(&event));
+}
+
+TEST(EventTests, checkEventClear)
+{
+    LAME_Event_Set(&event);
+    LAME_Event_Clear(&event);
+
+    CHECK_FALSE(LAME_Event_Check(&event));
+}
+
+TEST(EventTests, checkEventTakeAfterInit)
+{
+    CHECK_FALSE(LAME_Event_Take(&event));
+}
+
+TEST(EventTests, checkEventTakeAfterSetEvent)
+{
+    LAME_Event_Set(&event);
+    CHECK_TRUE(LAME_Event_Take(&event));
+}
+
+TEST(EventTests, checkEventAfterTake)
+{
+    LAME_Event_Set(&event);
+    LAME_Event_Take(&event);
     CHECK_FALSE(LAME_Event_Check(&event));
 }
 
 int main(int argc, char **argv)
 {
+    printf("\nEvent test");
     return RUN_ALL_TESTS(argc, argv);
 }
