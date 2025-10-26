0418864694b2cd2d914e3d0fc29e339b56d2e31a
polame <polame.dev@gmail.com>
2025-10-26 16:58:00 +0300

    Начальный файл SoftTimerTest

---
+++ tests/SoftTimerTest.cpp
@@ -0,0 +1,20 @@
+
+#include <CppUTest/TestHarness.h>
+
+TEST_GROUP(SoftTimer) {
+
+
+void setup()
+{
+}
+
+void teardown()
+{
+}
+
+};
+
+TEST(SoftTimer, clearAfterCreate)
+{
+    CHECK_TRUE(true);
+}
